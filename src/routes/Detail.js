5645317fd5d2d7004b0f1c2349d8edb3f42ff41a
bANg22ong <105845213+JooWn828@users.noreply.github.com>
2022-07-10 21:58:55 +0900

    220710 commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+function Detail() {
+  return (
+    <div>
+      <Link to='/'>
+        <h2>back</h2>
+      </Link>
+      <h1>Detail</h1>
+    </div>
+  );
+}
+export default Detail;
